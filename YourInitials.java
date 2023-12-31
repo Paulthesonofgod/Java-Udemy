803c20b3c22cac041cbdffbf6acd4e9a6dbe8ec1
Paul Ajolono Ogar <127955913+Paulthesonofgod@users.noreply.github.com>
2023-12-31 19:27:30 +0000

    Java work

---
+++ YourInitials.java
@@ -0,0 +1,12 @@
+public class YourInitials {
+    public static void main(String[] args) {
+         // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+         System.out.println("PPPP    OOO ");
+         System.out.println("P   P  O   O");
+         System.out.println("P   P  O   O");
+         System.out.println("PPPP   O   O");
+         System.out.println("P      O   O");
+         System.out.println("P      O   O");
+         System.out.println("P       OOO ");  
+    }
+}
